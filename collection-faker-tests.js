0f30ab68f7d788fcbef9429ddb78ac42e458b89b
macsj200 <maxjohansen@berkeley.edu>
2016-12-30 09:44:38 -0800

    init package

---
+++ collection-faker-tests.js
@@ -0,0 +1,11 @@
+// Import Tinytest from the tinytest Meteor package.
+import { Tinytest } from "meteor/tinytest";
+
+// Import and rename a variable exported by collection-faker.js.
+import { name as packageName } from "meteor/collection-faker";
+
+// Write your tests here!
+// Here is an example.
+Tinytest.add('collection-faker - example', function (test) {
+  test.equal(packageName, "collection-faker");
+});
